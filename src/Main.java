6db944f3d9c018e818cb1587c9e6e00ba994a4d8
nathanchow <nathannokhangc2027@student.cis.edu.hk>
2024-08-28 09:33:07 +0800

    IDEQuiz1.1

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,28 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Nathan
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println ("What is your age?");
+        age = scanner.nextInt();
+        System.out.println ("What is your first name?");
+        scanner.nextLine();
+        firstName = scanner.nextLine();
+        System.out.println ("What is your favourite food?");
+        favouriteFood = scanner.nextLine();
+        System.out.println ("Name: " + firstName + "\n\nAge: " + age + "\n\nFavourite food: " + favouriteFood);
+
+
+
 
 
 
